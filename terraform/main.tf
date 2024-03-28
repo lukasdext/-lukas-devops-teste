555a22f71c12e05335fd48dde1ad4c180bbc54f5
lukasdext <lukasdext160@hotmail.com>
2024-03-28 19:30:15 -0300

    - rodando pipe

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,8 +83,6 @@ resource "aws_ecs_task_definition" "my_task" {
 
 
 
-
-
 # Criação do serviço ECS
 resource "aws_ecs_service" "my_service" {
   name            = "my-service"
@@ -105,23 +103,18 @@ resource "aws_ecs_service" "my_service" {
   }
 }
 
-# Recursos adicionais para o Application Load Balancer (ALB)
 resource "aws_lb" "my_alb" {
   name               = "my-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.ecs_security_group.id]
-  subnets            = aws_subnet.my_subnets[*].id
-}
+  subnets            = [
+    aws_subnet.my_subnets[0].id,
+    aws_subnet.my_subnets[1].id,  # Adicione mais sub-redes conforme necessário
+  ]
 
-resource "aws_lb_listener" "my_alb_listener" {
-  load_balancer_arn = aws_lb.my_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.my_target_group.arn
+  tags = {
+    Name = "my-alb"
   }
 }
 
